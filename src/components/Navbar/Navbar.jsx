5cf4bdc30852148889e5b0c23e85b14fe0c3952a
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-07-19 12:01:04 -0600

    style: adding the page styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,48 @@
+import { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import 'bulma/css/bulma.min.css'
+import logo from '../../assets/graduacion.png'
+import './navbar.css'
 
+const Navbar = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const [isActive, setIsActive] = useState(false)
+
+  const toggleNavbar = () => {
+    setIsActive(!isActive)
+  }
+
+  return (
+    <div className="navbar-wrapper">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <button
+            className={`navbar-burger ${isActive ? 'is-active' : ''}`}
+            aria-label="menu"
+            aria-expanded={isActive ? 'true' : 'false'}
+            data-target="navbarBasicExample"
+            onClick={toggleNavbar}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </button>
+
+        </div>
+
+        <div className={`navbar-menu ${isActive ? 'is-active' : ''} is-left`}>
+          <div className="navbar-start">
+            <Link to="/">
+                <img className='logo' src={logo} alt="Logo" />
+            </Link>
+            <a className="navbar-item">About Me</a>
+            <a className="navbar-item">Documentation</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
