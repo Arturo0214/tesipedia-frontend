b73b162a26f9f15abd61e91840f06d4a695b24a6
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-08-02 22:40:37 -0600

    feat: added some pages to the main content

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Dashboard from "./pages/Dashboard/Dashboard"
 import './index.css'
 import Login from './pages/Login/Login'
 import Register from "./pages/Register/Register"
+import About from "./pages/About/About"
+import Work from "./pages/Work/Work"
+import Prices from "./pages/Prices/Prices"
 import {Routes, Route} from "react-router-dom"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -14,7 +17,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Dashboard/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/pricing' element={<Prices/>}/>
           <Route path='/register' element={<Register/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/work' element={<Work/>}/>
           <Route path='*' element={<h3>Page not found 404</h3>}/> 
         </Routes>
     <ToastContainer/>
---
+++ src/pages/Prices/Prices.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+const Prices = () => {
+  return (
+    <>
+    <header>
+      <Navbar/>
+    </header>
+    <main>  
+    <div>Prices</div>
+    </main>
+    <footer>
+      <Footer/>
+    </footer>
+    </>
+  )
+}
+
+export default Prices
