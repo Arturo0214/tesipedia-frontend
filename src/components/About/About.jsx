4de6781db95c0edae5db85a87032374313f6982c
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-08-09 13:13:15 -0600

    changed the dashboard page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,7 @@ import dos from '../../assets/no2.png'
 import tres from '../../assets/no3.png'
 import cuatro from '../../assets/no4.png'
 import cinco from '../../assets/no5.png'
+import hat from '../../assets/sombrero.png'
 
 
 const About = () => {
@@ -14,29 +15,59 @@ const About = () => {
         <div className='title'>
           <img src={logo}/>
         </div>
-          <h1>
+          <h1 className='kien'>
           ¿Quiénes somos?
           </h1>
         <div className="who container-fluid">
-          <p>
-            Tesipedia tiene como objetivo ayudar a todas aquellas personas que quieran alcanzar el éxito profesional
-            mediante la realización de trabajos de grado con profesionales dedicados especialmente a la investigación.
-          </p>
-          <p>
-            Estamos conscientes sobre los grandes desafíos que se pueden presentar cuando se quiere obtener un título
-            de grado y queremos ayudarte. No siempre es posible contar con el tiempo suficiente para poder concluir los trabajos de 
-            investigación.
-          </p>
-          <li>
-            Buscamos brindarte ese empujoncito hacia el éxito profesional, apoyándote con tu trabajo de grado.
-          </li>
-
+          <div className="quienes container-fluid">
+            <div className="quienes-texto">
+            <p className=''>
+              Tesipedia nace como una iniciativa para facilitar el proceso de titulación de todas aquellas personas que, 
+              por diversos motivos requieran apoyo externo en el desarrollo de sus trabajos de grado.
+            </p>
+            </div>
+            <div class="graduation-hat">
+                <img src={hat} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="claves">
+          <div className="vision">
+            <h3 className='titulo1'>
+              Misión
+            </h3>
+            <div className="texto-mision text-justify">
+             <p>
+             Nuestro propósito es respaldar integralmente a personas
+             que buscan el éxito en sus carreras profesionales, particularmente
+             en la etapa final de sus estudios, a través de la elaboración de 
+             sus tesis terminales o proyectos de grado. Nos comprometemos a 
+             brindar servicios de la más alta calidad, guiando a nuestros clientes 
+             en la investigación, análisis y presentación de trabajos académicos 
+             sobresalientes.
+             </p>
+            </div>
+          </div>
+          <div className="vision">
+            <h3 className='titulo2'>
+              Visión
+            </h3>
+            <div className="texto-vision text-justify">
+              <p>
+            Ser líderes reconocidos 
+            en toda latinoamérica en la asistencia y orientación para la realización de tesis y
+            trabajos de grado. Nuestra visión es ser una fuerza impulsora detrás del éxito 
+            profesional de innumerables individuos, brindándoles las herramientas necesarias para destacar 
+            en sus campos elegidos.
+            </p>
+            </div>
+          </div>
         </div>
         <div className="why container-fluid">
         <h3>
         ¿Por qué elegir Tesipedia?
         </h3>
-        <ul className='list'>
+        <ul className='list container-fluid'>
           <div className="left">
             <div className="filas">
               <div className="filaimg">
